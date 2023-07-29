2513bf6941bddd8c7a7bc06d97ddb66d118935ca
Gourav Sharma <gourav27012001@gmail.com>
2023-07-29 17:55:00 +0530

    projectile shooting completed by using clearRect and animate function requestAnimationFrame stops when tabs are changed we also calculated angle w.r.t centre using Math.atan2 function then calculated x and y coordinates using Math.cos and Math.sin respectively

--- index.js
+++ index.js
@@ -4,31 +4,79 @@ canvas.width = innerWidth; // innerWidth innerHeight are window object propertie
 canvas.height = innerHeight;
 
 //setting context
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
 
 //creating player
 
-class Player{
-    constructor(x, y, radius, color){
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.color = color;
-    }
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
 
-    draw (){
+  draw() {
+    ctx.beginPath(); //specifying to start drawing
+    ctx.arc(this.x, this.y, this.radius, 0, 360, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+}
 
-        ctx.beginPath();//specifying to start drawing
-        ctx.arc(this.x, this.y, this.radius, 0 ,360 ,false);
-        ctx.fillStyle = this.color;
-        ctx.fill();
+//creating projectile
 
-    }
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.velocity = velocity;
+    this.color = color;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, 360, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
 }
 
 //getting canvas center
-const x = canvas.width/2;
-const y = canvas.height/2;
+const x = canvas.width / 2;
+const y = canvas.height / 2;
 
-const player = new Player(x,y,20,'blue');
-player.draw();
+//drawing player
+const player = new Player(x, y, 20, "blue");
+
+//rendering prrojectile on click event
+const projectiles = [];
+
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+  projectiles.forEach((projectile) => {
+    projectile.update();
+  });
+}
+
+window.addEventListener("click", (event) => {
+  const angle = Math.atan2(
+    event.clientY - canvas.height / 2,
+    event.clientX - canvas.width / 2
+  ); //calculating angle of mouseClick
+  const velocity = { x: Math.cos(angle), y: Math.sin(angle) };
+
+  projectiles.push(
+    new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", velocity)
+  );
+});
+animate();
