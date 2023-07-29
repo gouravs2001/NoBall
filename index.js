8e6f5eeab25812a68a0a996daa955fd0df51649c
Gourav Sharma <gourav27012001@gmail.com>
2023-07-29 21:40:59 +0530

    created enemies using setInterval function in spawnEnemies

--- index.js
+++ index.js
@@ -49,6 +49,29 @@ class Projectile {
   }
 }
 
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.velocity = velocity;
+    this.color = color;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, 360, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+
 //getting canvas center
 const x = canvas.width / 2;
 const y = canvas.height / 2;
@@ -58,6 +81,28 @@ const player = new Player(x, y, 20, "blue");
 
 //rendering prrojectile on click event
 const projectiles = [];
+const enemies = [];
+
+function spawnEnemies() {
+  setInterval(() => {
+    let x, y;
+    const radius = Math.random() * (30 - 5) + 5; //create value between 5 and 30
+    let color = "green";
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+      y = Math.random() * canvas.height;
+    } else {
+      x = Math.random() * canvas.width;
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+    }
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+    enemies.push(new Enemy(x, y, radius, color, velocity));
+  }, 1000);
+}
 
 function animate() {
   requestAnimationFrame(animate);
@@ -66,6 +111,9 @@ function animate() {
   projectiles.forEach((projectile) => {
     projectile.update();
   });
+  enemies.forEach((enemy) => {
+    enemy.update();
+  });
 }
 
 window.addEventListener("click", (event) => {
@@ -73,10 +121,11 @@ window.addEventListener("click", (event) => {
     event.clientY - canvas.height / 2,
     event.clientX - canvas.width / 2
   ); //calculating angle of mouseClick
-  const velocity = { x: Math.cos(angle), y: Math.sin(angle) };
+  const velocity = { x: Math.cos(angle), y: Math.sin(angle) }; //calculating x and y components from angle for precise projection
 
   projectiles.push(
     new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", velocity)
   );
 });
+spawnEnemies();
 animate();
