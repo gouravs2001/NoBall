d8c58a54398bfcbb55ebe9aefcd0f09a469ac9b8
Gourav Sharma <gourav27012001@gmail.com>
2023-07-29 13:48:58 +0530

    Player class created

--- index.js
+++ index.js
@@ -2,3 +2,33 @@
 const canvas = document.querySelector("canvas");
 canvas.width = innerWidth; // innerWidth innerHeight are window object properties
 canvas.height = innerHeight;
+
+//setting context
+const ctx = canvas.getContext('2d');
+
+//creating player
+
+class Player{
+    constructor(x, y, radius, color){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw (){
+
+        ctx.beginPath();//specifying to start drawing
+        ctx.arc(this.x, this.y, this.radius, 0 ,360 ,false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+
+    }
+}
+
+//getting canvas center
+const x = canvas.width/2;
+const y = canvas.height/2;
+
+const player = new Player(x,y,20,'blue');
+player.draw();
